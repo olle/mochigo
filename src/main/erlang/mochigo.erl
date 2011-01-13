e3e22b5017a565829665f3a74d9016a2c696e71a
Olle Törnström <olle@studiomediatech.com>
2011-01-13 22:10:05 +0100

    Modified example output

--- src/main/erlang/mochigo.erl
+++ src/main/erlang/mochigo.erl
@@ -1,36 +1,15 @@
-%%% @author Olle Törnström <olle@studiomediatech.com>
-%%% @copyright (C) 2011, Olle Törnström
-%%% @doc
-%%% The entry module for my little application example, with an embedded
-%%% MochiWeb server.
-%%% @end
 -module(mochigo).
 -behaviour(application).
 -export([start/2, stop/1, response/1]).
 
-%% @doc
-%% Runs the mochigo application, starting a mochiweb server.
-%% @end
 start(_StartType, _StartArgs) ->
-    io:format("Starting mochigo...~n", []),
     {ok, Server} = mochiweb_http:start([{loop, {?MODULE, response}}]),
-    io:format("Server ~p started~n", [Server]),
     {ok, self(), Server}.
 
-%% @doc
-%% Stops the mochigo application and the mochiweb server.
-%% @end
 stop(Server) ->
-    io:format("Stopping mochigo server ~p~n", [Server]),
     mochiweb_http:stop(Server),
     ok.
 
-%% @private
-%% @doc
-%% Handles a web request, responds with some HTML output.
-%% @end
 response(Request) ->
-    "/" ++ Path = Request:get(path),
-    io:format("Serving request for path ~p~n", [Path]),
-    Request:ok({"text/plain", Path ++ ": foobar!"}).
+    Request:ok({"text/html", "<h1>MochiGo!</h1>"}).
 
